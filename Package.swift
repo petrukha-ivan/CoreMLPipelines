4bf07047818751aa01a14630a8af9db7c6a25da5
Petrukha Ivan <93926277+petrukha-ivan@users.noreply.github.com>
2025-03-24 12:35:14 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CoreMLPipelines",
-    platforms: [.iOS(.v18), .macOS(.v14)],
+    platforms: [.iOS(.v18), .macOS(.v15)],
     products: [
         .library(name: "CoreMLPipelines", targets: ["CoreMLPipelines"]),
         .executable(name: "coremlpipelines-cli", targets: ["CoreMLPipelinesCLI"])
